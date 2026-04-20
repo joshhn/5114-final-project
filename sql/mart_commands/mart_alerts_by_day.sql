9aac406c4ed7a6540ed7cd4922918cdc41d87082
eun1229 <eunhae.l@wustl.edu>
2026-04-20 16:12:28 -0500

    changes for alerts table

--- sql/mart_commands/mart_alerts_by_day.sql
+++ sql/mart_commands/mart_alerts_by_day.sql
@@ -2,33 +2,38 @@
 -- in the visualization, it probably makes the most sense to view this aggregated over all routes for a particular day, as a stacked bar chart for the type of alert counts.
 -- we could also use this to show which routes have the most alerts over a specified period of time
 
--- replace '2026-03-12' :service_date
+-- replace '2026-03-11' :service_date
 
 DELETE FROM LEMMING_DB.FINAL_PROJECT_MART.METRIC_ALERTS_BY_DAY
-WHERE alert_date = '2026-03-12';
+WHERE alert_date = '2026-03-11';
 
 INSERT INTO LEMMING_DB.FINAL_PROJECT_MART.METRIC_ALERTS_BY_DAY
 WITH active_on_date AS (
     SELECT
         far.entity_id,
         far.route_id,
+        r_dim.route_short_name as route_name,
         fa.severity_level,
-        '2026-03-12' AS alert_date
+        '2026-03-11' AS alert_date
     FROM LEMMING_DB.FINAL_PROJECT_FACT.FACT_ALERTS_ACTIVE_PERIODS fa_activep
     JOIN LEMMING_DB.FINAL_PROJECT_FACT.FACT_ALERTS_ROUTES far
         ON fa_activep.entity_id = far.entity_id
     JOIN LEMMING_DB.FINAL_PROJECT_FACT.FACT_ALERTS fa
         ON fa_activep.entity_id = fa.entity_id
+    JOIN LEMMING_DB.FINAL_PROJECT_STATIC.DIM_ROUTES r_dim -- get route name for dashboard
+      ON  far.route_id           = r_dim.route_id
+      AND fa.static_version_date = r_dim.feed_start_date
     WHERE far.route_type = 3
-      AND DATE(fa_activep.active_start) <= '2026-03-12'
-      AND (fa_activep.active_end IS NULL OR DATE(fa_activep.active_end) >= '2026-03-12')
+      AND DATE(fa_activep.active_start) <= '2026-03-11'
+      AND (fa_activep.active_end IS NULL OR DATE(fa_activep.active_end) >= '2026-03-11')
 )
 SELECT
     alert_date,
     route_id,
+    route_name,
     COUNT(DISTINCT entity_id)                                               AS alert_count,
     COUNT(DISTINCT CASE WHEN severity_level = 'SEVERE'  THEN entity_id END) AS severe_count,
     COUNT(DISTINCT CASE WHEN severity_level = 'WARNING' THEN entity_id END) AS warning_count,
     COUNT(DISTINCT CASE WHEN severity_level = 'INFO'    THEN entity_id END) AS info_count
 FROM active_on_date
-GROUP BY 1, 2;
+GROUP BY 1, 2, 3;
--- sql/table_creation_commands/create_mart_alerts_by_day.sql
+++ sql/table_creation_commands/create_mart_alerts_by_day.sql
@@ -5,6 +5,7 @@
 CREATE TABLE IF NOT EXISTS LEMMING_DB.FINAL_PROJECT_MART.METRIC_ALERTS_BY_DAY (
     alert_date       DATE,
     route_id         STRING,
+    route_name       STRING,
     alert_count      INT,
     severe_count     INT,
     warning_count    INT,
